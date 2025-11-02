c8cb426216191d61da44ba65da51937e61f2dafd
over <franciscosebastianyjunior@hotmail.com>
2025-11-02 18:30:43 -0300

    Actually understand the image processing being done and comment the main steps with your own words

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
+#include "opencv2/core/base.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char **argv) {
-    // 1) Lê em tons de cinza, independentemente do formato original
+    // Lê imagem em tons de cinza.
     cv::Mat imgGray = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
     if (imgGray.empty()) {
         std::cerr << "Erro: não foi possível carregar a imagem.\n";
         return -1;
     }
 
-    // 2) Detecta bordas com operador que gera valores negativos
-    // Laplaciano costuma dar valores < 0, então usamos CV_16S
+    // Gera imagem com bordas realçadas e fundo preto.
+    // CV_16S é usado pois o Laplaciano pode 1. gerar valores negativos 2. e valores maiores que 255.
+    // BORDER_CONSTANT expande a imagem com zeros nas bordas; aprendido em aula.
     cv::Mat edges16;
-    cv::Laplacian(imgGray, edges16, CV_16S, 3, 1, 0, cv::BORDER_DEFAULT);
+    cv::Laplacian(imgGray, edges16, CV_16S, 3, 0.5, 0, cv::BORDER_CONSTANT);
 
-    // 3) Converte para 8 bits absolutos para exibir
+    // Converte para CV_8U, necessidade de cv::imshow.
+    // Há a perda dos sinais negativos, que servem para indicar a direção da borda (claro -> escuro ou vice-versa).
     cv::Mat edges8;
     cv::convertScaleAbs(edges16, edges8);
 
-    // 4) Realça as bordas na imagem original
-    // soma simples: onde tem borda, fica mais claro
+    // Gera imagem similar à original, mas com as bordas realçadas.
     cv::Mat enhanced;
     cv::add(imgGray, edges8, enhanced, cv::noArray(), CV_8U);
 
-    cv::imshow("Original (tons de cinza)", imgGray);
+    cv::imshow("Original (Tons de Cinza)", imgGray);
     cv::imshow("Bordas (Laplaciano)", edges8);
-    cv::imshow("Imagem com bordas realcadas", enhanced);
+    cv::imshow("Bordas Realçadas", enhanced);
 
     cv::waitKey(0);
     return 0;
